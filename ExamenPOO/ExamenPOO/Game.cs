4b2bab1b354f2f66c1066b5b9eddb2134feee2d7
rcorrea4 <rcorrea4@miuandes.cl>
2019-11-21 17:49:31 -0300

    tercer commit examen

--- ExamenPOO/ExamenPOO/Game.cs
+++ ExamenPOO/ExamenPOO/Game.cs
@@ -18,7 +18,7 @@ namespace ExamenPOO
 
         Board board1 = new Board();
 
-        public void addBoat1()
+        public void addBoat1() //revisa largo tablero y si el bote fue puesto
         {
             Console.WriteLine("Que bote desea agregar?: 1.Portaaviones   2.Fragata   3.Submarino    4.Reparador   5.Radar   escribir string");
             int bote = Console.Read();
@@ -56,7 +56,7 @@ namespace ExamenPOO
 
 
         }
-        public void revTablero()
+        public void revTablero() //revisa que no halla un bote en el lugar y pone el bote si no hay
         {
             Console.WriteLine("Que bote desea agregar?: 1.Portaaviones   2.Fragata   3.Submarino    4.Reparador   5.Radar   escribir string");
             int bote = Console.Read();
@@ -67,8 +67,8 @@ namespace ExamenPOO
             Console.WriteLine("ingrese 0 para poner el bote horizontal y 1 para vertical");
             int orientacion = Console.Read();
 
-            int h = 0;
-            if (h == 0)
+            
+            if (orientacion== 0)
             {
                 if (posicionX + bote > 10)
                 {
@@ -104,13 +104,59 @@ namespace ExamenPOO
                     }
                 }
             }
-            else if (h == 1)
+            else if (orientacion == 1)
             {
                 if (posicionY + bote>10)
                 {
                     Console.WriteLine("pasa las coordenadas");
                 }
+                else
+                {
+                    if(board.vertical[posicionX] != 0)
+                    {
+                        int i = posicionY;
+                        int ocupado = 0;
+                        while (i < posicionY + bote)
+                        {
+                            ocupado = 0;
+                            if (board.vertical[i] != 0 && board.horizontal[posicionX] != 0)
+                            {
+                                Console.WriteLine("Espacio ocupado");
+                                ocupado = 1;
+                            }
+
+                        }
+                        if (ocupado != 1)
+                        {
+                            while (i < posicionY + bote)
+                            {
+                                board.vertical[i] = bote;
+                                board.horizontal[posicionX] = bote;
+
+                            }
+
+                        }
+
+                    }
+
+                }
             }
+        }
+        public void ataqueNormal(Board board2)
+        {
+
+            Console.WriteLine("Ingrese coordenadas de ataque, ingrese numeros de 1 a 10");
+            int x = Console.Read();
+            int y = Console.Read();
+
+            if (board2.horizontal[x]!=0 && board2.vertical[y]!=0 && board2.horizontal[x] == board2.vertical[x])
+            {
+                Console.WriteLine("Has derribado un bote");
+                board2.horizontal[x] = 0;
+                board2.vertical[y] = 0;
+            }
+                
+
         }
     }
 }
